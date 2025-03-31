28582fd779565e37430f0d007d5b52a05e52c036
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-03-31 07:15:54 +0530

    Ameet - Update AppController.java
    Update AppController.java for template

--- src/main/java/com/yaksha/assignment/controller/AppController.java
+++ src/main/java/com/yaksha/assignment/controller/AppController.java
@@ -6,21 +6,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
 public class AppController {
-
-	private final RestTemplate restTemplate;
-
-	public AppController(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
-	}
-
-	@PostMapping("/sendData")
-	public String sendPostRequest(@RequestParam String apiUrl, @RequestBody String requestData) {
-		// Sending the POST request to the external API (jsonplaceholder)
-		String response = restTemplate.postForObject(apiUrl, requestData, String.class);
-
-		// Return the response from the external API, along with the sent data
-		return "Data received: " + requestData + "\nResponse from API: " + response;
-	}
+	// write your logic here
 }
